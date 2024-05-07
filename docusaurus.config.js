e502c08e9205cbeccf8c57ec9d0f80b7977165e3
BijjuXD <103484184+bijju089@users.noreply.github.com>
2024-05-07 21:43:56 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -125,12 +125,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/mcst-hosting',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} MCST Hosting`,
       },
       prism: {
         theme: prismThemes.github,
