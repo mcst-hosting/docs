22e22d3cfa2b159b81e10544ae37caf8026287f8
BijjuXD <103484184+bijju089@users.noreply.github.com>
2024-05-07 20:55:25 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'MCST WiKi',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/mcstio.png',
 
   // Set the production url of your site here
   url: 'https://wiki.mcst.io',
